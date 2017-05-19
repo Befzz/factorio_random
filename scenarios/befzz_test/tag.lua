b67162a93d0845fe8affcc0ef6599f93796167b0
Andrey Balashov <tatergg@gmail.com>
2017-05-19 23:16:54 +0300

    0.1.5
    tooltips and more

--- scenarios/befzz_test/tag.lua
+++ scenarios/befzz_test/tag.lua
@@ -1,11 +1,16 @@
 -- Give players the option to set their preferred role as a tag
 -- A 3Ra Gaming creation
--- Befzz's revision 0.1.2
+-- Befzz's revision 0.1.5
+
+-- Role list: "tag_roles.lua"
+local roles = require "tag_roles"
+
+local expand_tag_gui
 
 local function create_tag_gui(event)
   local player = game.players[event.player_index]
-  if player.gui.top.tag == nil then
-    local button = player.gui.top.add { name = "tag", type = "sprite-button", caption = "Tag", style = "dialog_button_style" }
+  if player.gui.top.tag_toggle_btn == nil then
+    local button = player.gui.top.add { name = "tag_toggle_btn", type = "sprite-button", caption = "Tag", style = "dialog_button_style" }
     button.style.font = "default-bold"
     button.style.minimal_height = 38
     button.style.minimal_width = 38
@@ -18,23 +23,6 @@ local function create_tag_gui(event)
   end
 end
 
--- Tag list
--- Icon name must be legal. (right click on a Tag button to explore icon names)
-
-local roles = {
-  ["Trooper"] = "item/tank",
-  ["Security"] = "item/submachine-gun",
-  ["Resources"] = "item/iron-axe",
-  ["Production"] = "item/assembling-machine-2",
-  ["Science"] = "item/science-pack-1",  
-  ["Spaghetti"] = "item/transport-belt",
-  ["Trains"] = "item/locomotive",
-  ["Powah!"] = "item/steam-turbine",
-  ["Lurker"] = "entity/small-spitter",
-  ["Cat"] = "item/raw-fish"
-}
-
-
 
 -- Make a list of random names with roles
 local function test_fake_players( t )
@@ -45,7 +33,7 @@ local function test_fake_players( t )
     table.insert( roles_ind, role)
   end
   
-  for i = 1,limit do print(i) 
+  for i = 1,limit do
     local rolei = math.random(1, #roles_ind)
     local role = roles_ind[rolei]
 
@@ -62,27 +50,14 @@ local function test_fake_players( t )
   end
 end
 
-local function expand_tag_gui(player, devmode, dev_addfakes)
+local function get_random_from_table(tbl)
+  return tbl[math.random(1,#tbl)]
+end
 
-  local player_role = player.tag:sub(2,-2)
+-- dev_icons(ctrl + click): show icon-choose buttons
+-- dev_addfakes(alt + click): add random number of player names w/ color
+expand_tag_gui = function(player, dev_icons, dev_addfakes, right_click)
 
-  local players_by_role = {}
-  
-  for role in pairs(roles) do
-    players_by_role[role] = {}
-  end
-  
-  for _, oplayer in pairs(game.connected_players) do
-    local prole = oplayer.tag:sub(2,-2)
-    if prole ~= "" then
-      table.insert( players_by_role[prole], {oplayer.name, oplayer.color})
-    end
-  end
-  
-  if dev_addfakes then
-    test_fake_players(players_by_role)
-  end
-  
   local frame = player.gui.left["tag-panel"]
   if (frame) then
     frame.destroy()
@@ -91,25 +66,59 @@ local function expand_tag_gui(player, devmode, dev_addfakes)
       player.gui.center["textfield_item_icon_frame"].destroy()
     end
     if player.tag ~= "" then
+      player.gui.top.tag_toggle_btn.tooltip = "Tag: "..player.tag.."\n Right Click to show offline players with tags."
       if player.admin then
-        player.gui.top.tag.tooltip = "Tag: "..player.tag.."\n Right Click to debug icons."
-      else
-        player.gui.top.tag.tooltip = "Tag: "..player.tag
+        player.gui.top.tag_toggle_btn.tooltip = player.gui.top.tag_toggle_btn.tooltip.."\n CTRL + Click to explore icons.\n ALT + Click to add fake names"
       end
-      
     end
     return
   end
   
-  player.gui.top.tag.tooltip = ""
+  local player_role = player.tag:sub(2,-2)
+
+  -- Will be filled: { roleN = {{name,color},...} , ...}
+  local players_by_role = {}
   
-  local button
-  local frame = player.gui.left.add { type = "frame", direction = "vertical", name = "tag-panel", style = "frame_style", caption = "What are you doing right now:"}
+  for role in pairs(roles) do
+    players_by_role[role] = {}
+  end
   
-  if devmode then
+  if right_click then
+    for _, oplayer in pairs(game.players) do
+      local prole = oplayer.tag:sub(2,-2)
+      if prole ~= "" then
+        if oplayer.connected then
+          table.insert( players_by_role[prole], {oplayer.name, oplayer.color})
+        else
+          table.insert( players_by_role[prole], {oplayer.name, {r=0,g=0,b=0}})
+        end
+      end
+    end
+  else
+    for _, oplayer in pairs(game.connected_players) do
+      local prole = oplayer.tag:sub(2,-2)
+      if prole ~= "" then
+          table.insert( players_by_role[prole], {oplayer.name, oplayer.color})
+      end
+    end
+  end
+  
+
+  
+  if dev_addfakes then
+    test_fake_players(players_by_role)
+  end
+  
+  player.gui.top.tag_toggle_btn.tooltip = ""
+  
+  local button--reusable variable :D
+  local frame = player.gui.left.add { type = "frame", direction = "vertical", name = "tag-panel", caption = "What are you doing right now:"}
+  
+  if dev_icons then
     local choose
     local chooselist = frame.add { type = "flow", direction = "horizontal" }
-    for itype, ivalue in pairs({["item"] = "green-wire", ["entity"] = "medium-spitter", ["signal"] = {type = "virtual", name = "signal-A"}, ["tile"] = "grass"}) do
+    -- ["signal"] = {type = "virtual", name = "signal-A"}
+    for itype, ivalue in pairs({["item"] = "green-wire", ["entity"] = "medium-spitter", ["tile"] = "grass"}) do
       choose = chooselist.add { type = "choose-elem-button", elem_type = itype, [itype] = ivalue, name = "help_item_icon_choose_"..itype }
       choose.style.minimal_height = 36
       choose.style.minimal_width = 36
@@ -121,84 +130,105 @@ local function expand_tag_gui(player, devmode, dev_addfakes)
   end
   
   local scroll = frame.add{type = "scroll-pane", horizontal_scroll_policy = "never", vertical_scroll_policy = "auto"}
-  
-  --Frame max. height
   scroll.style.maximal_height = 600
   scroll.style.minimal_width = 250
   scroll.style.bottom_padding = 10
-  local table_roles = scroll.add{type = "table", name = "table_roles", colspan = 2}
-  table_roles.style.horizontal_spacing = 20
-  table_roles.style.vertical_spacing = 6
-
-  for role, role_icon in pairs(roles) do
   
-    local role_line = table_roles.add { type = "flow" }
-
-    button = role_line.add { type = "sprite-button", sprite = role_icon, name = role}
-
-    button.style.minimal_width = 50
-    button.style.minimal_height = 34
-    button.style.top_padding = 2
-    button.style.left_padding = 2
-    button.style.right_padding = 2
-    button.style.bottom_padding = 2
+  local table_roles = scroll.add{type = "table", name = "table_roles", colspan = 2}
+  table_roles.style.horizontal_spacing = 15
+  table_roles.style.vertical_spacing = 4
         
-    local sublabel = role_line.add { type = "label", caption = role}
-    -- sublabel.style.minimal_width = 0
-    sublabel.style.minimal_height = 36
-    sublabel.style.top_padding = 2
-    sublabel.style.left_padding = 0
-    sublabel.style.right_padding = 0
-    sublabel.style.bottom_padding = 0
-    sublabel.style.font = "default-bold"
+        
+  local name_label
+  local color_k = 0.6
+  local pname
+  local pcolor
+  
+  local show_role_tooltip = math.random() > .5
+  
+  for role, role_icons in pairs(roles) do
+  
+    local role_line = table_roles.add { type = "flow", direction = "horizontal" }
+
+    button = role_line.add { type = "sprite-button", sprite = get_random_from_table(role_icons), name = "tag_role_"..role, style = "recipe_slot_button_style"}
+    button.style.top_padding = 4
+    button.style.left_padding = 0
+    button.style.right_padding = 0
+    button.style.bottom_padding = 4
+    if show_role_tooltip and role_icons.tooltip then
+      button.tooltip = get_random_from_table( role_icons.tooltip )
+    end
+    
+    local role_cap_line = role_line.add { type = "flow", direction = "horizontal" }
+    role_cap_line.style.max_on_row = 1
+         
+    local role_label = role_cap_line.add { type = "label", caption = role, single_line = true}
+    -- role_label.style.minimal_width = 0
+    role_label.style.minimal_height = 0
+    role_label.style.maximal_height = 12  
+    role_label.style.top_padding = 0
+    role_label.style.left_padding = 0
+    role_label.style.right_padding = 0
+    role_label.style.bottom_padding = 0
+    role_label.style.font = "default-bold"
     if role == player_role then
-      sublabel.style.font_color = {r=.7,g=1,b=.7}
+      role_label.style.font_color = {r=.7,g=1,b=.7}
+    end
+    
+    
+    if #players_by_role[role] > 0 then
+      local count_label = role_cap_line.add {type = "label", caption = #players_by_role[role], single_line = false}
+      count_label.style.font = "default-small-bold"
+      count_label.style.maximal_height = 10  
+      count_label.style.top_padding = 0
+      count_label.style.left_padding = 0
+      count_label.style.right_padding = 0
+      count_label.style.bottom_padding = 0
+      count_label.style.font_color = {r=.55,g=.55,b=.55}
     end
 
 
-    local plist = table_roles.add { type = "flow", direction = "horizontal" }
-    plist.style.max_on_row = 3
-    plist.style.top_padding = 0
-    plist.style.bottom_padding = 7
-    -- plist.style.vertical_spacing = 0
-    local pbutn
-    local color_k = 0.6
-    local pname
-    local pcolor
+    local list_players = table_roles.add { type = "flow", direction = "horizontal" }
+    list_players.style.max_on_row = 3
+    list_players.style.top_padding = 0
+    list_players.style.bottom_padding = 7
+    -- list_players.style.vertical_spacing = 0
+
     if players_by_role[role] then
       for _,pdata in pairs(players_by_role[role]) do
         pname = pdata[1]
         pcolor = pdata[2]
         
-        pbutn = plist.add {type = "label", caption = pname, want_ellipsis = true, single_line = false}
-        pbutn.style.font = "default"
-        pbutn.style.top_padding = 0
-        pbutn.style.right_padding = 4
-        pbutn.style.left_padding = 1
-        pbutn.style.bottom_padding = 0
-        pbutn.style.maximal_height = 11
-        pbutn.style.minimal_height = 11
-        pbutn.style.maximal_width = 120
-        pbutn.style.minimal_width = 120
-        
-
-        pbutn.style.font_color = {
+        name_label = list_players.add {type = "label", caption = pname, want_ellipsis = true, single_line = false}
+        name_label.style.font = "default"
+        name_label.style.top_padding = 0
+        name_label.style.right_padding = 4
+        name_label.style.left_padding = 2
+        name_label.style.bottom_padding = 0
+        name_label.style.maximal_height = 11
+        name_label.style.minimal_height = 11
+        name_label.style.maximal_width = 120
+        name_label.style.minimal_width = 120
+        name_label.style.font_color = {
          r = .4 + pcolor.r * color_k,
          g = .4 + pcolor.g * color_k,
          b = .4 + pcolor.b * color_k,
         }
+
       end
     end
   end
   
-  button = frame.add { type = "button", caption = "Clear", name = "tag_clear" }
+  
+  button = frame.add { type = "button", caption = "Clear tag", name = "tag_clear" }
   button.style.font = "default-bold"
   if player.tag == "" then
     button.style.font_color = {r=.7, g=1, b=.7}
+    button.caption = "Close"
   else
     button.style.font_color = {r=1, g=0.5, b=0.5}
   end
-  button.style.minimal_width = 60
+  button.style.minimal_width = 80
   button.style.maximal_height = 26
   button.style.top_padding = 0
   button.style.left_padding = 2
@@ -207,29 +237,11 @@ local function expand_tag_gui(player, devmode, dev_addfakes)
 end
 
 local function update_player_role(player, role)
-  -- local prev_role = player.tag:sub(2,-2)
-  
-  -- if prev_role ~= "" then
-    -- global.players_by_role[prev_role][player.index] = nil
-  -- end
-  
   if role ~= "" then
-    -- if global.players_by_role[role] == nil then
-      -- global.players_by_role[role] = {player.index}
-    -- else
-      -- global.players_by_role[role][player.index] = 1
-    -- end
     player.tag = "[" .. role .. "]"
   else
     player.tag = ""
   end
-end
-
-local function on_player_left_game(event)
-  local player = game.players[event.player_index]
-  
-  update_player_role(player, "")
-  
 end
 
 
@@ -237,34 +249,44 @@ local function on_gui_click(event)
   if not (event and event.element and event.element.valid) then return end
   local player = game.players[event.element.player_index]
   local name = event.element.name
-  local devmode = (event.button == defines.mouse_button_type.right)
-  if (name == "tag") then
-    expand_tag_gui(player, devmode, event.alt and player.admin)
+  if (name == "tag_toggle_btn") then
+    --player, dev_icons, dev_addfakes
+    expand_tag_gui(player, 
+      player.admin and event.control, 
+      player.admin and event.alt,
+      event.button == defines.mouse_button_type.right)
   end
 
   if (name == "tag_clear") then
   
     update_player_role(player, "")
     
-    player.gui.top.tag.caption = "Tag"
-    player.gui.top.tag.tooltip = ""
-    player.gui.top.tag.sprite = ""
+    player.gui.top.tag_toggle_btn.caption = "Tag"
+    player.gui.top.tag_toggle_btn.tooltip = ""
+    player.gui.top.tag_toggle_btn.sprite = ""
     expand_tag_gui(player)
     return
   end
-  for role, role_icon in pairs(roles) do
-    if (name == role) then
-    
-      update_player_role(player, role)
-        
-      player.gui.top.tag.caption = ""
-      player.gui.top.tag.sprite = role_icon
+  
+  --role button clicked
+  if name:find("tag_role_") == 1 then
+    local _,role_ind_start = name:find("tag_role_")
+    local name_role = name:sub(role_ind_start + 1)
+    for role, role_icons in pairs(roles) do
+      if (name_role == role) then
       
-      expand_tag_gui(player, devmode)
+        update_player_role(player, role)
+          
+        player.gui.top.tag_toggle_btn.caption = ""
+        player.gui.top.tag_toggle_btn.sprite = event.element.sprite  --get_random_from_table(role_icons)
+        
+        expand_tag_gui(player)
+      end
     end
   end
 end
 
+--handle choose-item button
 local function on_gui_elem_changed(event)
   if not (event and event.element and event.element.valid) then return end
   local player = game.players[event.element.player_index]
@@ -277,18 +299,19 @@ local function on_gui_elem_changed(event)
       local frame = player.gui.center.add{ type = "frame", name = "textfield_item_icon_frame", caption = "SpritePath"}
       frame.style.minimal_width = 310
       local textfield 
-      if type(event.element.elem_value ) == 'table' then
-        textfield = frame.add { name = "textfield_item_icon", type = "textfield", text = "virtual-signal/" .. event.element.elem_value.name }
-      else
+      -- if type(event.element.elem_value ) == 'table' then
+        -- textfield = frame.add { name = "textfield_item_icon", type = "textfield", text = "virtual-signal/" .. event.element.elem_value.name }
+      -- else
         textfield = frame.add { name = "textfield_item_icon", type = "textfield", text = event.element.elem_type .. "/" .. event.element.elem_value }
-      end
+      -- end
+      
+      --buggy
       textfield.style.minimal_width = 300
     end
   end
   
 end
 
-Event.register(defines.events.on_player_left_game, on_player_left_game)
 Event.register(defines.events.on_gui_elem_changed, on_gui_elem_changed)
 Event.register(defines.events.on_gui_click, on_gui_click)
 Event.register(defines.events.on_player_joined_game, create_tag_gui)
---
+++ scenarios/befzz_test/tag_roles.lua
@@ -0,0 +1,178 @@
+-- A part of tag.lua
+
+return {
+  ["Trooper"] = {
+    "item/tank",
+    "item/exoskeleton-equipment",
+    "item/power-armor-mk2",
+    "item/energy-shield-mk2-equipment",
+    "item/battery",
+    "item/battery-mk2-equipment",
+    tooltip = {
+      "Incoming!!!",
+      "If the facts don't fit the theory, change the facts.",
+      "I suppose it is tempting, if the only tool you have is a hammer, to treat everything as if it were a nail.",
+      "There's a fine line between genius and insanity. I have erased this line."
+    }
+  },
+  ["Security"] = {
+    "item/gun-turret",
+    "item/laser-turret",
+    "item/atomic-bomb",
+    "item/firearm-magazine",
+    "item/piercing-rounds-magazine",
+    "item/flamethrower-turret",
+    "item/stone-wall",
+    "item/pistol",
+    tooltip = {
+      "I like guns.",
+      "Robocop!", 
+      "Attack is the best form of defense.",
+      "Only the insecure strive for security."
+      }
+  },
+  ["Resources"] = {
+    "item/steel-axe",
+    "item/iron-axe",
+    "item/iron-ore",
+    "item/copper-ore",
+    tooltip = {
+      "Mine or not to mine", 
+      "The nation behaves well if it treats the natural resources as assets which\n it must turn over to the next generation increased, and not impaired, in value."
+      }
+  },
+  ["Production"] = {
+    "item/assembling-machine-1",
+    "item/assembling-machine-2",
+    "item/assembling-machine-3",
+    "item/inserter",
+    "item/stack-inserter",
+    tooltip = {
+      "When every physical and mental resources is focused, one's power to solve a problem multiplies tremendously.",
+      "The production of too many useful things results in too many useless people. ",
+      "Everything must be made as simple as possible. But not simpler."
+      }
+  },
+  ["Science"] = {
+    "item/science-pack-1",
+    "item/science-pack-2",
+    "item/science-pack-3",
+    "item/military-science-pack",
+    "item/production-science-pack",
+    "item/high-tech-science-pack",
+    "item/space-science-pack",
+    tooltip = {
+      "Science without religion is lame, religion without science is blind",
+      "If we knew what it was we were doing, it would not be called research, would it?",
+      "Somewhere, something incredible is waiting to be known.",
+      "I'm sure the universe is full of intelligent life. It's just been too intelligent to come here."
+    }
+  },
+  ["Spaghetti"] = {
+    "item/transport-belt",
+    "item/fast-transport-belt",
+    "item/express-transport-belt",
+    "item/fast-underground-belt",
+    "item/fast-splitter",
+    "item/splitter",
+    "item/express-splitter",
+    tooltip = {
+      "Naturally italian",
+      '"A long, thin, cylindrical, solid pasta"(wiki)'
+      }
+    
+  },
+  ["Trains"] = {
+    "entity/curved-rail",
+    "item/cargo-wagon",
+    "item/fluid-wagon",
+    "item/locomotive",
+    "item/rail-signal",
+    "item/rail",
+    tooltip = {
+      "Ch, ch, choooo!",
+      "The only way of catching a train I have ever discovered is to miss the train before. ",
+      "If a trainstation is where the train stops, what's a workstation...?"
+      }
+  },
+  ["Powah!"] = {
+    "item/steam-turbine",
+    "item/nuclear-reactor",
+    "item/heat-exchanger",
+    "item/steam-engine",
+    "item/solar-panel",
+    "item/accumulator",
+    "item/boiler",
+    "fluid/steam",
+    tooltip = {
+      "I ve Got The power!",
+      "Power Overwhelming!!!111",
+      "Its Over 9000!!!",
+      '"If you want to find the secrets of the universe, think in terms of energy, frequency and vibration." (Tesla)'
+      }
+  },
+  ["Lurker"] = {
+    "item/night-vision-equipment",
+    "entity/construction-robot",
+    "item/wooden-chest",
+    "item/stone-brick",
+    "item/distractor-capsule",
+    "entity/dry-tree",
+    "entity/tree-09-red",
+    "entity/stone-rock",
+    "entity/green-coral",
+    "entity/player",
+    "entity/tree-05",
+    tooltip = {
+      "Not here",
+      "I just watch",
+      "I am an owl, bird of the night. I see everything. I know everything.",
+      "A day without sunshine is like, you know, night.",
+      "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+      }
+  },
+  ["Oil"] = {
+    "item/storage-tank",
+    "item/pipe-to-ground",
+    "item/oil-refinery",
+    "item/chemical-plant",
+    "item/pumpjack",
+    "fluid/crude-oil",
+    "fluid/heavy-oil",
+    "fluid/light-oil",
+    "fluid/petroleum-gas",
+    "fluid/lubricant",
+    "fluid/sulfuric-acid",
+    tooltip = {
+      "We're running out of oil!",
+      "Black gold",
+      "Naturally occurring, yellow-to-black liquid found in geological formations beneath the Earth's surface, which is commonly refined into various types of fuels.",
+      "Components of petroleum are separated using a technique called fractional distillation.",
+      "The petroleum industry generally classifies crude oil by the geographic location it is produced in (e.g. West Texas Intermediate, Brent, or Oman), its API gravity (an oil industry measure of density), and its sulfur content."
+    }
+  },
+  ["Cat"] = {
+    "item/raw-fish",
+    tooltip = {
+      "=^.^=",
+      "Meow",
+      "In a cat's eye, all things belong to cats.",
+      "Cats don't like change without their consent.",
+      "Heard melodies are sweet, but those unheard, are sweeter",
+      "God does not play dice with the universe"
+    }
+  },
+  ["Dog"] = {
+    "entity/small-biter",
+    "entity/medium-biter",
+    "entity/big-biter",
+    "entity/behemoth-biter",
+    tooltip = {
+      "Not a cat",
+      "Friend",
+      "It's not the size of the dog in the fight, it's the size of the fight in the dog.",
+      "When what you want is a relationship, and not a person, get a dog",
+      "A dog has one aim in life... to bestow his heart."
+      }
+  }
+}
